8c4ea22b96607529a033daa69eb014227cafda0e
aseppp <saepudinasep86@gmail.com>
2023-02-06 10:55:50 +0700

    cord

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 // const morgan = require("morgan");
 const express = require("express");
+const cors = require("cors");
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 // app.use(morgan.token)
 const router = require("./routes/index");
