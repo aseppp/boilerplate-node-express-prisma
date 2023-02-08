a9d6b6419124423589e64026616a7baf3750c821
aseppp <saepudinasep86@gmail.com>
2023-02-08 13:39:48 +0700

    adujustment index

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 const router = require("./routes/index");
 app.use("/api", router);
 
-app.use(cors());
 app.get("/", (req, res) => {
   res.json("Server working properly!");
 });
